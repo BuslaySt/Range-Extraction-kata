c3699a6c7a89229d3627085a938abcf0ea317b15
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-06-22 22:48:35 +0300

    init

---
+++ test_kata.py
@@ -0,0 +1,10 @@
+from kata import *
+import unittest
+
+
+class Test_Range_Extraction(unittest.TestCase):
+    def test0(self):    
+        self.assertEqual(0, 1)
+
+if __name__ == '__main__':
+    unittest.main()
